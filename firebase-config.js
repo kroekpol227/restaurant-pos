103b60dc00dd32412425204db448b9e79a546486
Kroekpol <kroekpool.cpk@gmail.com>
2026-09-16 13:57:11 +0700

    two  commit

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,19 @@
-// Import Firebase SDK แบบ CDN (ใช้งานง่าย ไม่ต้องลง Node modules สำหรับฝั่ง Frontend ล้วน)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+// Import Firebase SDK และ Firestore
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCA2ZiY5Gv-72PqgFlFXYbaTav9BF-LeQ8",
+  authDomain: "my-restaurant-pos-62a74.firebaseapp.com",
+  projectId: "my-restaurant-pos-62a74",
+  storageBucket: "my-restaurant-pos-62a74.firebasestorage.app",
+  messagingSenderId: "610934330593",
+  appId: "1:610934330593:web:29db39b405100c0ec253a0"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export ฐานข้อมูลเพื่อนำไปใช้ในไฟล์อื่น (script.js และ admin.js)
 export const db = getFirestore(app);
